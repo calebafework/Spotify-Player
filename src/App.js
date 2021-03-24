f3429c89a1864113c5c9971540a7d2428bbf3365
calebafework <calebafework@gmail.com>
2021-03-24 13:39:24 -0600

    working player, no progress bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import * as $ from "jquery";
+import { authEndpoint, clientId, redirectUri, scopes } from "./config";
+import hash from "./Hash";
+import Player from "./Player";
+import logo from "./baddoglogo.svg";
+import "./App.css";
 
-function App() {
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      token: null,
+    item: {
+      album: {
+        images: [{ url: "" }]
+      },
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
+    },
+    is_playing: "Paused",
+    progress_ms: 0,
+    no_data: false,
+    };
+
+  this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
+  this.tick = this.tick.bind(this);
+  }
+  
+  componentDidMount(){
+    //set token
+    let _token = hash.access_token;
+    if (_token) {
+      //set token 
+      this.setState({token: _token});
+    }
+    this.interval = setInterval(() => this.tick(), 5000)
+  }
+
+  componentWillUnmount() {
+    // clear the interval to save resources
+    clearInterval(this.interval);
+  }
+
+  tick() {
+    if(this.state.token) {
+      this.getCurrentlyPlaying(this.state.token);
+    }
+  }
+
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: xhr => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        if(!data){
+          this.setState({
+            no_data: true,
+          })
+          return 
+        }
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+          no_data: false,
+        });
+      }
+    });
+  }
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {!this.state.token && (
+          <a
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                "%20"
+              )}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
+          </a>
+        )}
+        {this.state.token && !this.state.no_data && (
+          <Player
+            item={this.state.item}
+            is_playing={this.state.is_playing}
+            progress_ms={this.progress_ms}
+          />
+        )}
+        {this.state.no_data && (
+            <p>
+              You need to be playing a song on Spotify, for something to appear here.
+            </p>
+          )}
       </header>
     </div>
-  );
+  )
+  }
 }
-
 export default App;
---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+export const clientId = "d3af59447a1b44f5a020458436b40a83";
+export const redirectUri  = 'http://localhost:3000';
+export const scopes = [
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
+
